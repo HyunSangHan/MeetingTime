f48e4ac020cb0a1dfd4abcb3a371cef9a2234e29
HyunSangHan <gustkd3@gmail.com>
2019-03-15 00:01:49 +0900

    190315 Edit: controller to return json type

--- controller.rb
+++ controller.rb
@@ -4,27 +4,154 @@ require './db_class.rb'
 require './function.rb'
 enable :sessions
 
-###################################################################################################
 
-# <NOTICE>: 'check session' is a justifed function!
+############# Question: How to get params in using React?
 
-# [Function]
-# : check_session, get_meeting_info, join, assign_first_score, get_ranking_result, get_cutline, use_heart, use_cash
+DEFAULT_SCORE_RATE = 1000
+COUNT_LIMIT = 9999
 
-# [Controller]
-# : POST - login_process, sign_up_process, edit_my_info_process, invite, find_lost_password
-# : GET - login, sign_up, heart_payment, cash_payment, logout, secession, get_matching_result, get_my_info
 
-# and..... please think more
-###################################################################################################
+# get '/' do 
+#     if !session["user_id"].nil?
+#         redirect '/main'
+#     else
+#         erb :initpage
+#     end
+# end
 
-get '/' do 
-    if !session["user_id"].nil?
-        redirect '/main'
-    else
-        erb :initpage
-    end
+get '/get_user_info' do
+    user = User.find(session["user_id"])
+    return user.to_json
 end
+
+get '/get_meeting_info_first' do
+    meeting = MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
+                            .where("starting_date < ?", Time.now().to_datetime).take
+    return meeting.to_json
+end
+
+get '/get_meeting_info_cutline' do    
+    meeting = MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
+                            .where("starting_date < ?", Time.now().to_datetime).take
+    all_users = meeting.joined_users
+                          
+    female_count = all_users.where(:is_male => false).count
+    male_count = all_users.where(:is_male => true).count
+    cutline = [female_count, male_count].min
+    meeting.cutline = cutline
+    meeting.save
+
+    return meeting.to_json
+end
+
+get '/get_joined_user_info' do
+    user = User.find(session["user_id"])
+    meeting = MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
+                            .where("starting_date < ?", Time.now().to_datetime).take
+    my = JoinedUser.where("user_id" => user.id)
+                    .where("meeting_detail_id" => meeting.id).take
+
+    if my.nil?
+        my = JoinedUser.new    
+        my.user = user
+        my.total_score = 0 # will be added when making cutline 1000
+        my.meeting_detail_id = meeting
+        my.is_male = user.is_male
+        my.is_deleted = false
+
+        all_users = meeting.joined_users
+        male_count = all_users.where(:is_male => true).count
+        female_count = all_users.where(:is_male => false).count
+
+        if user.is_male == true
+            my.ranking = male_count + 1
+            my.midranking = male_count + 1
+        else
+            my.ranking = female_count + 1
+            my.midranking = female_count + 1
+        end
+        my.save
+    else
+        puts "You already joined this meeting. Please wait"
+    end
+
+    return my.to_json
+end
+
+get '/assign_first_score' do
+# MUST check if all cases are included
+    meeting = MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
+                            .where("starting_date < ?", Time.now().to_datetime).take
+    all_users = meeting.joined_users
+    cutline = meeting.cutline
+
+    i = cutline
+    count = 0 # for making break condition
+    while 0 < i # for winner
+        count = count + 1 
+        break if count > COUNT_LIMIT
+        my = all_users.where("ranking" => i).take
+        my.total_score = DEFAULT_SCORE_RATE + (cutline - my.ranking).to_i * DEFAULT_SCORE_RATE
+        my.save
+        i -= 1
+    end
+
+    i = cutline 
+    count = 0
+    while i < cutline + DEFAULT_SCORE_RATE # for loser
+        count = count + 1 
+        break if count > COUNT_LIMIT
+        i += 1
+        my = all_users.where("ranking" => i).take
+        my.total_score = DEFAULT_SCORE_RATE - (my.ranking - cutline).to_i #check if it's int or string
+        break if my.total_score == 0
+        my.save 
+    end
+
+    return all_users.to_json
+end
+
+get '/get_my_partner' do
+    # if session["user_id"].nil?
+    #     redirect '/'
+    # else
+    user = User.find(session["user_id"])
+    meeting = MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
+                            .where("starting_date < ?", Time.now().to_datetime).take
+
+    my = JoinedUser.where("user_id" => user.id)
+                    .where("meeting_detail_id" => meeting.id).take
+
+    all_users = meeting.joined_users
+    male_users = all_users.where(:is_male => true).take
+    female_users = all_users.where(:is_male => false).take
+
+    male_users.order("total_score DESC").each_with_index do |xy, i| ###total score???
+        xy.ranking = i + 1
+        xy.save
+    end
+
+    female_users.order("total_score DESC").each_with_index do |xx, j|
+        xx.ranking = j + 1
+        xx.save
+    end
+
+    #if ########### have to edit about 'last' meeting, and run this 'if' meeting was finished only 
+    if meeting.cutline >= my.ranking
+        if my.is_male?
+            my_partner = female_users.where("ranking" => my.ranking).take
+        else
+            my_partner = male_users.where("ranking" => my.ranking).take
+        end
+    else
+        puts "sorry. you are loser!" #need to be edited
+    end
+
+    return my_partner.to_json
+end
+################################################################################
+
+
 
 post '/signin_process' do
     user = User.where("email" => params["email"]).first
@@ -63,69 +190,39 @@ post '/signup_process' do
     end 
 end
 
-get '/sign_up' do
-    erb :sign_up
-end
 
-get '/sign_out' do
-    session.clear
-    redirect '/'
-end
+# get '/sign_up' do
+#     erb :sign_up
+# end
 
-get '/main' do
-    if session["user_id"].nil?
-        redirect '/'
-    else
-    @user = User.find(session["user_id"])
-     erb :main
-    end
-end    ##################### need to add get_ranking_result function with "if" about DateTime
-  
-get '/matching_result' do
-    # if session["user_id"].nil?
-    #     redirect '/'
-    # else
-    @user = User.find(session["user_id"])
-    meeting = get_meeting_info
+# get '/sign_out' do
+#     session.clear
+#     redirect '/'
+# end
 
-    my = JoinedUser.where("user_id" => user.id)
-                    .where("meeting_detail_id" => meeting.id).take
+# get '/main' do
+#     if session["user_id"].nil?
+#         redirect '/'
+#     else
+#     @user = User.find(session["user_id"])
+#      erb :main
+#     end
+# end
 
-    all_users = meeting.joined_users
-    male_users = all_users.where(:is_male => true).take
-    female_users = all_users.where(:is_male => false).take
+# get '/delete/:user_id' do 
+#     user = User.find(session["user_id"])
+#     user.delete
+#     redirect '/'
+# end
 
-    make_ranking_result
-
-    #if ########### have to edit about 'last' meeting, and run this 'if' meeting was finished only 
-    if meeting.cutline >= my.ranking
-        if my.is_male?
-            my_partner = female_users.where("ranking" => my.ranking).take
-        else
-            my_partner = male_users.where("ranking" => my.ranking).take
-        end
-    else
-        puts "sorry. you are loser!" #need to be edited
-    end
-
-    erb :matching_result #need to add my_partner
-    # end
-end
-
-get '/delete/:user_id' do 
-    user = User.find(session["user_id"])
-    user.delete
-    redirect '/'
-end
-
-get '/profile' do
-    if session["user_id"].nil?
-        redirect '/'
-    else
-        @user = User.find(session["user_id"])
-        erb :profile
-    end
-end
+# get '/profile' do
+#     if session["user_id"].nil?
+#         redirect '/'
+#     else
+#         @user = User.find(session["user_id"])
+#         erb :profile
+#     end
+# end
     
 post '/edit_profile' do 
     user = User.find(session["user_id"])
@@ -141,23 +238,23 @@ post '/edit_profile' do
     redirect '/profile'
   end  
 
-get '/buy_heart' do
-    if session["user_id"].nil?
-        redirect '/'
-    else
-        @user = User.find(session["user_id"])
-        erb :buy_heart
-    end
-end
+# get '/buy_heart' do
+#     if session["user_id"].nil?
+#         redirect '/'
+#     else
+#         @user = User.find(session["user_id"])
+#         erb :buy_heart
+#     end
+# end
 
-get '/chats' do
-    if session["user_id"].nil?
-        redirect '/'
-    else
-        @user = User.find(session["user_id"])
-        erb :chats
-    end
-end
+# get '/chats' do
+#     if session["user_id"].nil?
+#         redirect '/'
+#     else
+#         @user = User.find(session["user_id"])
+#         erb :chats
+#     end
+# end
 
 
 # get '/cash_payment' do 
@@ -240,51 +337,4 @@ end
 #     user.save
 
 #     redirect '/'
-# end
-
-
-
-# get '/get_matching_result' do
-#     check session
-
-# end
-
-# get '/get_my_info' do
-#     check session
-# end
-
-# post '/edit_my_info_process' do
-#     check session
-
-#     if params["password"].nil?
-#         redirect '/error1_2' # Enter Password
-
-#     elsif params["password_confirm"].nil?
-#         redirect '/error1_3' # Enter Password_confirm
-
-#     if params["password"].length < 6
-#         redirect '/error1_9' # Password should be longer than 6 syllables
-#     end
-
-#     if params["password"] != params["password_confirm"]
-#         redirect '/error1_10' # Check the Password
-
-#     elsif
-#         user.company = Company.find(params["company_name"]) #right?
-#         user.nickname = params["nickname"]
-#         user.email = params["email"]
-#         user.phone_number = params["phone_number"]
-#         user.password = BCrypt::Password.create(params["password"])
-#         user.location = params["location"]
-#         user.team_detail = params["team_datail"]
-#         user.profile_img = params["profile_img"]
-#         user.save
-
-#         redirect '/'
-#     end
-# end
-    
-# post '/invite' do
-#     check_session
-
 # end
--- function.rb
+++ function.rb
@@ -17,10 +17,8 @@ enable :sessions
 # and..... please think more
 ###################################################################################################
 
-DEFAULT_SCORE_RATE = 100
 CASH_TO_HEART = 1
 HEART_TO_SCORE = 1000
-COUNT_LIMIT = 9999
 private
 def check_session 
     return User.find(session["user_id"])
@@ -30,98 +28,9 @@ def get_meeting_info
     MeetingDetail.where("meeting_date > ?", Time.now().to_datetime)
                   .where("starting_date < ?", Time.now().to_datetime).take
 end
+#####
 
-def join_clicked
-    user = check_session
-    meeting = get_meeting_info
-    if user.nil?
-        redirect '/' #redirection should be in Sinatra files.
-    else
-        joined_user = JoinedUser.new    
-        joined_user.user = user
-        joined_user.total_score = 0 # will be added when making cutline
-        joined_user.meeting_detail_id = meeting
-        joined_user.is_deleted = false
 
-        all_users = meeting.joined_users
-        male_count = all_users.where(:is_male => true).count
-        female_count = all_users.where(:is_male => false).count
-
-        if user.is_male == true
-            joined_user.ranking = male_count
-            joined_user.midranking = male_count
-        else
-            joined_user.ranking = female_count
-            joined_user.midranking = female_count
-        end
-        joined_user.save
-    end
-    return joined_user.ranking
-end
-
-def assign_first_score # MUST check if all cases are included
-    meeting = get_meeting_info
-    all_users = meeting.joined_users
-    cutline = meeting.cutline
-
-    i = cutline
-    count = 0
-    while 0 < i # for winner
-        count = count + 1 
-        break if count > COUNT_LIMIT
-        my = all_users.where("ranking" => i).take
-        my.score = DEFAULT_SCORE_RATE + (cutline - my.ranking).to_i * DEFAULT_SCORE_RATE
-        my.save
-        i -= 1
-    end
-
-    i = cutline 
-    count = 0
-    while i < cutline + DEFAULT_SCORE_RATE # for loser
-        count = count + 1 
-        break if count > COUNT_LIMIT
-        i += 1
-        my = all_users.where("ranking" => i).take
-        my.score = DEFAULT_SCORE_RATE - (my.ranking - cutline).to_i #check if it's int or string
-        break if my.score == 0
-        my.save 
-    end
-end
-
-def make_ranking_result
-    meeting =  get_meeting_info #have to edit! (have to get last meeting)
-    all_users = meeting.joined_users
-    male_users = all_users.where(:is_male => true)
-    female_users = all_users.where(:is_male => false)
-
-    male_users.order("total_score DESC").each_with_index do |xy, i|
-        xy.ranking = i + 1
-        xy.save
-    end
-
-    female_users.order("total_score DESC").each_with_index do |xx, j|
-        xx.ranking = j + 1
-        xx.save
-    end
-    
-    return all_users
-end
-
-def get_cutline    
-    meeting = get_meeting_info
-    all_users = meeting.joined_users
-    #doo2's comment ----> all_users = JoinedUser.where(:meeting_detail_id = meeting.id)
-                          
-    female_count = all_users.where(:is_male => false).count
-    male_count = all_users.where(:is_male => true).count
-    puts female_count
-    puts male_count
-    cutline = [female_count, male_count].min
-    meeting.cutline = cutline
-    meeting.save
-
-    return cutline
-end
 
 def use_heart heart_payment_params
     user = check_session
