265885ed7781ec88ee9ccc62ff701e5ead4be6b9
Taeyoung Jang <taeyoungjang@Taeyoungui-MacBook-Pro.local>
2019-07-13 13:21:16 +0900

    post_save import 추가

--- backend/meeting/models.py
+++ backend/meeting/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.db.models.signals import post_save
 
 class Meeting(models.Model):
     open_time = models.DateTimeField(blank=False, null=False)
