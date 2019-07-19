22048f1d29b70bee483951c8e166bd8628c00e4d
HYUNSANG HAN <44132406+HyunSangHan@users.noreply.github.com>
2019-07-19 22:52:43 +0900

    urls.py urlpatterns에서 include 기본경로 api/ 제거(불필요)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,6 @@ router.register('meeting_info', views.MeetingInfoView, 'meeting_info')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/current_meeting/', views.CurrentMeeting.as_view())
+    path('/', include(router.urls)),
+    path('current_meeting/', views.CurrentMeeting.as_view())
 ]
