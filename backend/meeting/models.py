60a0fd2c0c28293313b48b439cde2a4a2cef8f92
HyunSangHan <gustkd3@gmail.com>
2019-07-20 21:05:40 +0900

    중복코드 제거

--- backend/meeting/models.py
+++ backend/meeting/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from django.db.models.signals import post_save
 from faker import Faker #for debugging later
 
 class Meeting(models.Model):
