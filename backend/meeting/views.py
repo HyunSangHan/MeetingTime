6f74f5980bf2dd0b070c6459dfca831df7989d20
Jaewoo Maeng <maeng9584@gmail.com>
2019-07-22 20:08:59 +0900

    DELETE 추가, post 셔플 수정, patch 문법수정

--- backend/meeting/views.py
+++ backend/meeting/views.py
@@ -11,33 +11,52 @@ class MeetingInfoView(viewsets.ModelViewSet):
     serializer_class = MeetingSerializer
     queryset = Meeting.objects.all()
 
-class MatchingAPI(APIView):
+class CurrentMatching(APIView):
 
     def post(self, request, format=None):
-        joined_users_male = list(JoinedUser.objects.filter(is_matched=False and is_female=False))
-        joined_users_female = list(JoinedUser.objects.filter(is_matched=False and is_female=True))
-        random.shuffle(joined_users_male)
-        random.shuffle(joined_users_female)
+        joined_users_male = list(JoinedUser.objects.filter(is_matched=False, profile__is_male=True))
+        joined_users_female = list(JoinedUser.objects.filter(is_matched=False, profile__is_male=False))
+        numbers = list(range(len(joined_users_male)))
+        random.shuffle(numbers)
+        matched = list()
+
         for i in range(len(joined_users_male)):
-            serializer = MatchingSerializer(Matching.objects.create(joined_male=joined_users_male[i], joined_female=joined_users_female[i]))
+            runned = 1
+            while runned < 3:
+                if joined_users_male[i].already_met_one != joined_users_female[numbers[0]].rank:
+                    #matched.append
+                    serializer = MatchingSerializer(Matching.objects.create(joined_male=joined_users_male[i], joined_female=joined_users_female[numbers[0]], trial_time=request.data.trial_time))
+                    numbers[0].pop()
+                    break
+                else:
+                    random.shuffle(numbers)
+                    runned += 1
+
             if serializer.is_valid():
                 serializer.save()
-            else
+            else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, statuso=status.HTTP_201_CREATED)
 
     def patch(self, request, format=None):
         queryset = Matching.objects.filter(id=request.data.id)
-        serializer = MatchingSerializer(queryset)
-        serializer.is_greenlight_male = request.data.is_greenlight_male
-        serializer.is_greenlight_female = request.data.is_greenlight_female
-        serializer.is_gift_male = request.data.is_gift_male
-        serializer.is_gift_female = request.data.is_gift_female
+        serializer = MatchingSerializer(queryset, data=request.data, partial=True)
         if serializer.is_greenlight_male and serializer.is_greenlight_female:
-            serializer.kakao_chattingroom = "kakao_chat_example/1"
+            matched_matching = {
+                "kakao_chattingroom" = "kakao_chat_example/1"
             # kakao 채팅방을 최대 50개를 준비해놓고 ChattingRoom 이런 model에 저장해놓은 다음 filter 해서 할당되지 않은 첫번째
             # url을 할당하는 것도 괜찮을 것 같네요 (50개는 우리가 매주 admin으로 입력해놓고)
+            }
+            serializer = MatchingSerializer(queryset, data=matched_matching, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, format=None):
+        if request.data.trail_time > 0:
+            Matching.objects.filter(trail_time=request.data.trial_time, is_matched=False).delete()
+        else:
+            Matching.objects.all().delete()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
