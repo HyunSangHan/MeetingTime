9eb8f7a08a0f015f8c0b5cc1e58556f0a1bca80b
choiyeonseok <smr603@snu.ac.kr>
2019-08-18 07:48:21 +0900

    counter-player 렌더링

--- frontend/src/components/details/CounterPlayer.js
+++ frontend/src/components/details/CounterPlayer.js
@@ -19,29 +19,39 @@ class CounterPlayer extends Component {
     
     
     render() {
-        const {  } = this.props;  
+        const { counter_profile } = this.props;  
 
         
         return (
             <div className="counter-container">
+                    <div className="team-info">
+                        <div className="team-name">
+                            {counter_profile.team_name}
+                        </div>
+                        <div className="age-range">
+                            {counter_profile.age_range}
+                        </div>
+                        <div className="company">
+                            {counter_profile.company.name}
+                        </div>
+                    </div>
+                    <div className="team-intro font-notosan">
+                        <p>{counter_profile.team_introduce}</p>
+                    </div>
+                    <div className="images">
+                        <img src={counter_profile.image || require("./../../images/noPhoto.jpg")} />
+                        <img src={counter_profile.image_two || require("./../../images/noPhoto.jpg")} />
+                        <img src={counter_profile.image_three || require("./../../images/noPhoto.jpg")} />
+                    </div>
 
-                    <h2>counter profile입니다.</h2>
-                    {/* <div className="column">
-                        <img src={counter_profile.image || require("./../../images/noPhoto.jpg")}
-                            alt={counter_profile.user.username}
-                            className="main-profile" width="100px"
-                        />
-                        <ul className="info-list">
+                        {/* <ul className="info-list">
                             <li className="list-item">이름 : {counter_profile.user.username}</li>
                             <li className="list-item">성별 : {counter_profile.is_male ? "남" : "여"}</li>
                             <li className="list-item">연령대 : {counter_profile.age_range ? counter_profile.age_range + "대" : "몰라요"}</li>
                             <li className="list-item">회사명 : {counter_profile.company.name}</li>
                             <li className="list-item">팀소개 : {counter_profile.team_introduce}</li>
-                        </ul>
-                    </div>
-                <br/> */}
-                
-                
+                        </ul> */}
+                   
             </div>
         )
     };
