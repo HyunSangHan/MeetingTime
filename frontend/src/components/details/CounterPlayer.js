47e77a1c4133f0757de64aeb12f3c3848b478056
choiyeonseok <smr603@snu.ac.kr>
2019-08-30 14:51:13 +0900

    main profile images-wrapper 수정

--- frontend/src/components/details/CounterPlayer.js
+++ frontend/src/components/details/CounterPlayer.js
@@ -23,49 +23,49 @@ class CounterPlayer extends Component {
 
         
         return (
-            <div className="counter-container">
-                    <div className="team-info">
-                        <div className="team-name font-notosan">
-                            {counter_profile.team_name}
+            <div className="total-container">
+                <div className="counter-container">
+                        <div className="team-info">
+                            <div className="team-name font-notosan">
+                                {counter_profile.team_name}
+                            </div>
+                            <div className="age-range font-notosan">
+                                {counter_profile.age_range}
+                            </div>
+                            <div className="company font-notosan">
+                                {counter_profile.company.name}
+                            </div>
                         </div>
-                        <div className="age-range font-notosan">
-                            {counter_profile.age_range}
+                        <div className="team-intro font-notosan">
+                            <p>{counter_profile.team_introduce}</p>
                         </div>
-                        <div className="company font-notosan">
-                            {counter_profile.company.name}
-                        </div>
-                    </div>
-                    <div className="team-intro font-notosan">
-                        <p>{counter_profile.team_introduce}</p>
-                    </div>
-                
-                    <div className="margin-wrap">
-                        <div className="images-wrap">
+
+                        <div className="margin-wrap" />
+                </div>
+                <div className="images-wrapper">
                             <div className="images">
                                 <div className="each-image flex-center" >
                                     <div className="App">
                                         <img src={counter_profile.image || require("./../../images/noPhoto.jpg")} />
                                     </div>
                                 </div>
-                        
+
                                 <div className="each-image flex-center" >
                                     <div className="App">
                                         <img src={counter_profile.image_two || require("./../../images/noPhoto.jpg")} />
                                     </div>
                                 </div>
-                        
+
 
                                 <div className="each-image flex-center" >
                                     <div className="App">
                                         <img src={counter_profile.image_three || require("./../../images/noPhoto.jpg")} />
                                     </div>
                                 </div>
-                    
+
                                 <div className="last-child-gap"/>
                             </div>
-                        </div>
-                    </div>
-                   
+                </div>
             </div>
         )
     };
