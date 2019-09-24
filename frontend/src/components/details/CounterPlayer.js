8ca8f8f474a90cc09e9aa1c7e9bf9278daff83e9
HyunSangHan <gustkd3@gmail.com>
2019-09-24 20:53:56 +0900

    ToolTipDown 도입, css 수정

--- frontend/src/components/details/CounterPlayer.js
+++ frontend/src/components/details/CounterPlayer.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import '../../css/Main.scss'; //부모컴포넌트의CSS(SCSS)
 import '../../App.css'; //공통CSS
+import ToolTipDown from './ToolTipDown';
 
 class CounterPlayer extends Component {
     render() {
         const { counter_profile } = this.props;  
         return (
             <div className="total-container top-m">
+                <ToolTipDown content={ "테스트" }/>
                 <div className="counter-container">
                     <div className="team-info">
                         <div className="team-name font-notosan">
