ed3e55fab783071439aae54a23a362401ce65304
HyunSangHan <gustkd3@gmail.com>
2019-12-29 23:57:28 +0900

    Make loading component spin

--- frontend/src/components/details/Loading.js
+++ frontend/src/components/details/Loading.js
@@ -2,7 +2,7 @@ import React from "react"
 import "../../css/Reuse.scss" //도구성컴포넌트 CSS
 
 const Loading = props => (
-  <div className="container">
+  <div className="container spinner">
     <img
       src={require("../../images/loading.png")}
       style={{ width: "30px" }}
