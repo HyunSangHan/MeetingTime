91eb778c2a9bd6c85b3efc065f3cb7199e093bd9
HyunSangHan <gustkd3@gmail.com>
2020-03-30 00:54:10 +0900

    Fix admin readable

--- server/meeting/models.py
+++ server/meeting/models.py
@@ -8,17 +8,21 @@ from allauth.account.signals import user_signed_up
 
 
 class Meeting(models.Model):
-    open_time = models.DateTimeField()
-    prev_meeting_last_result_time = models.DateTimeField(null=True)
-    close_time = models.DateTimeField()
-    first_shuffle_time = models.DateTimeField()
-    second_shuffle_time = models.DateTimeField()
-    third_shuffle_time = models.DateTimeField()
-    last_result_time = models.DateTimeField(null=True)
-    meeting_time = models.DateTimeField()
-    location = models.CharField(max_length=20)
-    cutline = models.IntegerField(null=True, blank=True)
-    description = models.CharField(max_length=20, null=True, blank=True)
+    open_time = models.DateTimeField(help_text="미팅 선착순이 열리는 시간")
+    prev_meeting_last_result_time = models.DateTimeField(null=True, help_text="지난번 미팅매칭 프로세스가 종료되었던 시간")
+    close_time = models.DateTimeField(help_text="미팅 선착순이 닫히는 시간")
+    first_shuffle_time = models.DateTimeField(help_text="1번째 섞는 시간 / 즉, first met에서 second met으로 넘어가는 시간")
+    second_shuffle_time = models.DateTimeField(help_text="2번째 섞는 시간 / 즉, second met에서 third met으로 넘어가는 시간")
+    third_shuffle_time = models.DateTimeField(help_text="3번째 섞는 시간 / 즉, third met에서 last met으로 넘어가는 시간")
+    last_result_time = models.DateTimeField(null=True, help_text="모든 미팅매칭 프로세스가 종료되는 시간")
+    meeting_time = models.DateTimeField(help_text="실제 미팅 만나는 오프라인 약속 기준 시간")
+    location = models.CharField(max_length=20, help_text="실제 미팅 만나는 오프라인 약속 기준 장소")
+    cutline = models.IntegerField(null=True, blank=True, help_text="남 vs 여 중 적은 숫자의 매칭지원자수")
+    description = models.CharField(max_length=20, null=True, blank=True, help_text="미팅 공지문")
+
+    class Meta:
+        verbose_name = '미팅 모집(Meeting)'
+        verbose_name_plural = '미팅 모집(Meeting)'
 
     def __str__(self):
         return f'미팅장소: {str(self.location)} / 오픈일자: {str(self.open_time).split(" ")[0]} / 미팅일자: {str(self.meeting_time).split(" ")[0]}'
@@ -91,6 +95,10 @@ class Company(models.Model):
     name = models.CharField(max_length=20, blank=True)
     domain = models.CharField(max_length=100, blank=True)
 
+    class Meta:
+        verbose_name = '등록된 회사(Company)'
+        verbose_name_plural = '등록된 회사(Company)'
+
     def __str__(self):
         return self.name
 
@@ -108,6 +116,10 @@ class Profile(models.Model):
     team_introduce = models.TextField(blank=True, default="")
     last_intro_modified_at = models.DateTimeField(null=True, blank=True)
     validated = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = '회원/팀정보(Profile)'
+        verbose_name_plural = '회원/팀정보(Profile)'
 
     def __str__(self):
         return self.user.username
@@ -162,12 +174,20 @@ class JoinedUser(models.Model):
     already_met_two = models.IntegerField(null=True, blank=True)
     already_met_three = models.IntegerField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = '미팅지원자 현황(JoinedUser)'
+        verbose_name_plural = '미팅지원자 현황(JoinedUser)'
+
     def __str__(self):
         return f'{self.profile.user.username} (is_male: {self.profile.is_male})'
 
 class KakaoChatting(models.Model):
     chattingroom_url = models.URLField(null=True, blank=True)
     is_used = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = '카카오 채팅방 URL(KakaoChatting)'
+        verbose_name_plural = '카카오 채팅방 URL(KakaoChatting)'
 
     def __str__(self):
         return self.chattingroom_url
@@ -181,6 +201,10 @@ class Matching(models.Model):
     is_gift_male = models.BooleanField(default=False)
     is_gift_female = models.BooleanField(default=False)
     kakao_chattingroom = models.ForeignKey(KakaoChatting, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '미팅 모집 내 개별 각각의 매칭(Matching)'
+        verbose_name_plural = '미팅 모집 내 각각의 개별 매칭(Matching)'
 
     def seed(num_of_seed):
         import random
@@ -205,3 +229,7 @@ class Matching(models.Model):
 class Validation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     code = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = '이메일 인증코드(Validation)'
+        verbose_name_plural = '이메일 인증코드(Validation)'
